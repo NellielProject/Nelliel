92b451daad73a52f528550c53ee81c755963cf27
OtakuMegane <otakumegane@insaneotaku.com>
2017-12-26 16:15:57 -0500

    Cleanup unused

--- board_files/include/initializations.php
+++ board_files/include/initializations.php
@@ -31,7 +31,6 @@ $enabled_types = array();
 
 $dataforce['page_gen'] = 'main';
 $dataforce['archive_update'] = FALSE;
-$dataforce['post_links'] = '';
 $dataforce['sp_field1'] = (!empty($_POST[nel_stext('TEXT_SPAMBOT_FIELD1')])) ? $_POST[nel_stext('TEXT_SPAMBOT_FIELD1')] : NULL;
 $dataforce['sp_field2'] = (!empty($_POST[nel_stext('TEXT_SPAMBOT_FIELD2')])) ? $_POST[nel_stext('TEXT_SPAMBOT_FIELD2')] : NULL;
 $dataforce['mode'] = NULL;
@@ -43,15 +42,9 @@ if (!empty($_POST))
     $dataforce['mode'] = (isset($_POST['mode'])) ? $_POST['mode']: NULL;
     $dataforce['admin_pass'] = (isset($_POST['super_sekrit'])) ? $_POST['super_sekrit'] : NULL;
     $dataforce['username'] = (isset($_POST['username'])) ? $_POST['username'] : NULL;
-    $dataforce['usrdel'] = (isset($_POST['usrdel'])) ? $_POST['usrdel'] : NULL;
     $dataforce['expand_thread'] = (isset($_POST['expand_thread'])) ? $_POST['expand_thread'] : NULL;
     $dataforce['delpost'] = (isset($_POST['delpost'])) ? TRUE : FALSE;
     $dataforce['banpost'] = (isset($_POST['banpost'])) ? TRUE : FALSE;
-    $dataforce['banid'] = (isset($_POST['banid']) && is_numeric($_POST['banid'])) ? (int) $_POST['banid'] : NULL;
-    $dataforce['banreason'] = (isset($_POST['banreason'])) ? $_POST['banreason'] : NULL;
-    $dataforce['banip'] = (isset($_POST['ban_ip'])) ? $_POST['ban_ip'] : NULL;
-    $dataforce['timedays'] = (isset($_POST['timedays'])) && is_numeric($_POST['timedays']) ? (int) $_POST['timedays'] : NULL;
-    $dataforce['timehours'] = (isset($_POST['timehours'])) && is_numeric($_POST['timehours']) ? (int) $_POST['timehours'] : NULL;
     $dataforce['response_to'] = (isset($_POST['response_to']) && is_numeric($_POST['response_to'])) ? (int) $_POST['response_to'] : NULL;
     $dataforce['only_delete_file'] = (isset($_POST['onlyimgdel'])) ? TRUE : FALSE;
 }
@@ -65,7 +58,6 @@ if (!empty($_GET))
     $dataforce['response_id'] = (isset($_GET['post']) && is_numeric($_GET['post'])) ? (int) $_GET['post'] : NULL;
 }
 
-$link_updates = '';
 $fgsfds = array('noko' => FALSE, 'noko_topic' => 0, 'sage' => FALSE, 'sticky' => FALSE);
 $link_resno = 0;
 
