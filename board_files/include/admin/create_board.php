09fb792440e9f23fe192171f70aaf83e2c5e4cab
OtakuMegane <otakumegane@insaneotaku.com>
2018-02-27 17:47:03 -0500

    Update setup routines

--- board_files/include/admin/create_board.php
+++ board_files/include/admin/create_board.php
@@ -11,26 +11,9 @@ function nel_create_new_board()
     $board_id = $_POST['new_board_id'];
     $board_directory = $_POST['board_directory'];
     $db_prefix = $board_id;
-
     $prepared = $dbh->prepare('INSERT INTO "' . BOARD_DATA_TABLE . '" ("board_id", "board_directory", "db_prefix") VALUES (?, ?, ?)');
     $dbh->executePrepared($prepared, array($board_id, $board_directory, $db_prefix));
-    $references = nel_board_references($board_id);
-
-    nel_create_posts_table($references['post_table']);
-    nel_create_posts_table($references['archive_post_table']);
-    nel_create_threads_table($references['thread_table']);
-    nel_create_threads_table($references['archive_thread_table']);
-    nel_create_files_table($references['file_table']);
-    nel_create_files_table($references['archive_file_table']);
-    nel_create_config_table($references['config_table']);
-
-    $file_handler->createDirectory($references['src_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['thumb_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['page_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['archive_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['archive_src_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['archive_thumb_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['archive_page_path'], DIRECTORY_PERM, true);
-
+    nel_create_board_tables($board_id);
+    nel_create_board_directories($board_id);
     return $board_id;
 }
--- board_files/include/setup/setup.php
+++ board_files/include/setup/setup.php
@@ -17,42 +17,16 @@ function setup_check($board_id) // TODO Do this better
         die('InnoDB engine is required for MySQL support. However the engine has been disabled for some stupid reason. We can\'t function like this.');
     }
 
-    $file_handler = nel_file_handler();
-
-    nel_create_bans_table(BAN_TABLE);
-    nel_create_user_table(USER_TABLE);
-    nel_create_roles_table(ROLES_TABLE);
-    nel_create_user_role_table(USER_ROLE_TABLE);
-    nel_create_permissions_table(PERMISSIONS_TABLE);
-    nel_create_logins_table(LOGINS_TABLE);
-    nel_create_board_data_table(BOARD_DATA_TABLE);
-    nel_create_site_config_table(SITE_CONFIG_TABLE);
-
-    $file_handler->createDirectory(CACHE_PATH, DIRECTORY_PERM, true);
+    nel_create_core_tables();
+    nel_create_core_directories();
 
     if (true_empty($board_id))
     {
         return;
     }
 
-    $references = nel_board_references($board_id);
-
-    nel_create_threads_table($references['thread_table']);
-    nel_create_threads_table($references['archive_thread_table']);
-    nel_create_posts_table($references['post_table'], $references['thread_table']);
-    nel_create_posts_table($references['archive_post_table'], $references['archive_thread_table']);
-    nel_create_files_table($references['file_table'], $references['post_table']);
-    nel_create_files_table($references['archive_file_table'], $references['archive_post_table']);
-    nel_create_board_config_table($references['config_table']);
-
-    $file_handler->createDirectory($references['src_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['thumb_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['page_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['archive_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['archive_src_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['archive_thumb_path'], DIRECTORY_PERM, true);
-    $file_handler->createDirectory($references['archive_page_path'], DIRECTORY_PERM, true);
-
+    nel_create_board_tables($board_id);
+    nel_create_board_directories($board_id);
     nel_setup_stuff_done('check_done_nochange');
 
     if (STUFF_DONE === true)
@@ -77,6 +51,48 @@ function nel_setup_stuff_done($status)
     }
 }
 
+function nel_create_core_directories()
+{
+    $file_handler = nel_file_handler();
+    $file_handler->createDirectory(CACHE_PATH, DIRECTORY_PERM, true);
+}
+
+function nel_create_core_tables()
+{
+    nel_create_bans_table(BAN_TABLE);
+    nel_create_user_table(USER_TABLE);
+    nel_create_roles_table(ROLES_TABLE);
+    nel_create_user_role_table(USER_ROLE_TABLE);
+    nel_create_permissions_table(PERMISSIONS_TABLE);
+    nel_create_logins_table(LOGINS_TABLE);
+    nel_create_board_data_table(BOARD_DATA_TABLE);
+    nel_create_site_config_table(SITE_CONFIG_TABLE);
+}
+
+function nel_create_board_directories($board_id)
+{
+    $file_handler = nel_file_handler();
+    $references = nel_board_references($board_id);
+    $file_handler->createDirectory($references['src_path'], DIRECTORY_PERM, true);
+    $file_handler->createDirectory($references['thumb_path'], DIRECTORY_PERM, true);
+    $file_handler->createDirectory($references['page_path'], DIRECTORY_PERM, true);
+    $file_handler->createDirectory($references['archive_path'], DIRECTORY_PERM, true);
+    $file_handler->createDirectory($references['archive_src_path'], DIRECTORY_PERM, true);
+    $file_handler->createDirectory($references['archive_thumb_path'], DIRECTORY_PERM, true);
+    $file_handler->createDirectory($references['archive_page_path'], DIRECTORY_PERM, true);
+}
+
+function nel_create_board_tables($board_id)
+{
+    $references = nel_board_references($board_id);
+    nel_create_threads_table($references['thread_table']);
+    nel_create_threads_table($references['archive_thread_table']);
+    nel_create_posts_table($references['post_table'], $references['thread_table']);
+    nel_create_posts_table($references['archive_post_table'], $references['archive_thread_table']);
+    nel_create_files_table($references['file_table'], $references['post_table']);
+    nel_create_files_table($references['archive_file_table'], $references['archive_post_table']);
+    nel_create_board_config_table($references['config_table']);
+}
 
 function nel_check_for_innodb()
 {
