e14cdf8be5fc98ddd478152f779f85328c682e49
OtakuMegane <otakumegane@insaneotaku.com>
2018-03-07 01:01:23 -0500

    Some config defaults added, a bit of comment updates

--- board_files/include/initializations.php
+++ board_files/include/initializations.php
@@ -4,24 +4,34 @@ if (!defined('NELLIEL_VERSION'))
     die("NOPE.AVI");
 }
 
-define('INPUT_BOARD_ID', isset($_POST['board_id']) ? $_POST['board_id'] : '');
-define('WEB_FILES', 'web/'); // Name of directory where the support and internal files go
-define('IMAGES_DIR', WEB_FILES . 'imagez/'); // Web location of the javascript files
-define('CSS_DIR', WEB_FILES . 'css/'); // Web location of the css files
-define('JS_DIR', WEB_FILES . 'js/'); // Web location of the javascript files
+define('INPUT_BOARD_ID', isset($_POST['board_id']) ? $_POST['board_id'] : ''); // Default board id setting
+define('WEB_FILES', 'web/'); // Directory for CSS, Javascript and other web-related support files
+define('IMAGES_DIR', WEB_FILES . 'imagez/'); // Images used by Nelliel go here
+define('CSS_DIR', WEB_FILES . 'css/'); // CSS files here
+define('JS_DIR', WEB_FILES . 'js/'); // Javascript files here
 
 define('PHP_SELF', 'imgboard.php'); // Name of main script file
 define('PHP_SELF2', 'index'); // Name of board index
 define('PHP_EXT', '.html'); // Extension used for board pages
 
-define('BAN_TABLE', 'nelliel_bans'); // Table containing ban info
-define('USER_TABLE', 'nelliel_users'); // Table used for post data
-define('ROLES_TABLE', 'nelliel_roles'); // Table used for post data
-define('USER_ROLE_TABLE', 'nelliel_user_role'); // Table used for post data
-define('PERMISSIONS_TABLE', 'nelliel_permissions'); // Table used for post data
-define('LOGINS_TABLE', 'nelliel_login_attempts'); // Table used for post data
-define('BOARD_DATA_TABLE', 'nelliel_board_data'); // Table used for post data
-define('SITE_CONFIG_TABLE', 'nelliel_site_config'); // Table containing site-wide config
+define('BAN_TABLE', 'nelliel_bans'); // Contains ban info
+define('USER_TABLE', 'nelliel_users'); // Table of users
+define('ROLES_TABLE', 'nelliel_roles'); // Table of roles
+define('USER_ROLE_TABLE', 'nelliel_user_role'); // Which role is assigned to a user
+define('PERMISSIONS_TABLE', 'nelliel_permissions'); // Permissions
+define('LOGINS_TABLE', 'nelliel_login_attempts'); // Record of failed login attempts
+define('BOARD_DATA_TABLE', 'nelliel_board_data'); // Basic data on each board
+define('SITE_CONFIG_TABLE', 'nelliel_site_config'); // Site-wide config settings
+
+// Set default values here in case the config is missing something
+$base_config['defaultadmin'] = '';
+$base_config['defaultadmin_pass'] = '';
+$base_config['tripcode_salt'] = 'sodiumz';
+$base_config['run_setup_check'] = true;
+$base_config['directory_perm'] = '0775';
+$base_config['file_perm'] = '0664';
+$crypt_config['password_bcrypt_cost'] = 12;
+$crypt_config['password_sha2_cost'] = 200000;
 
 require_once CONFIG_PATH . 'config.php';
 
