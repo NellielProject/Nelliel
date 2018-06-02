85051981f9ab300f4311c340a8c9b3885af6a6ef
OtakuMegane <otakumegane@insaneotaku.com>
2018-06-02 19:48:59 -0400

    SHA256 for files is on by default now

--- board_files/include/setup/insert_data.php
+++ board_files/include/setup/insert_data.php
@@ -300,7 +300,7 @@ function nel_insert_board_config_defaults($config_table)
                         ('board_setting', 'nelliel', 'general', 'str', 'date_separator', '/'),
                         ('board_setting', 'nelliel', 'general', 'str', 'fgsfds_name', 'FGSFDS'),
                         ('board_setting', 'nelliel', 'general', 'str', 'indent_marker', '>>'),
-                        ('board_setting', 'nelliel', 'general', 'bool', 'file_sha256', '0'),
+                        ('board_setting', 'nelliel', 'general', 'bool', 'file_sha256', '1'),
                         ('filetype_enable', 'nelliel', 'graphics', 'bool', 'graphics', '1'),
                         ('filetype_enable', 'nelliel', 'graphics', 'bool', 'jpeg', '1'),
                         ('filetype_enable', 'nelliel', 'graphics', 'bool', 'gif', '1'),
