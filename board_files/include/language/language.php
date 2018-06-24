32da63ca17bb32ec52404e6a5dead89ef0d55dde
OtakuMegane <otakumegane@insaneotaku.com>
2018-06-24 16:27:52 -0400

    Remove unused language function

--- board_files/include/language/language.php
+++ board_files/include/language/language.php
@@ -40,25 +40,6 @@ function nel_extract_language($file)
     $file_handler->writeFile($file, $extractor->assemblePoString());
 }
 
-function nel_get_language($language, $form, $text)
-{
-    static $lang_arrays;
-
-    if (!isset($lang_arrays[$language]))
-    {
-        include LANGUAGE_PATH . 'lang.' . $language . '.php';
-        $lang_arrays[$language]['singular'] = $lang_singular;
-        $lang_arrays[$language]['plural'] = $lang_plural;
-    }
-
-    if (!isset($lang_arrays[$language][$form][$text]))
-    {
-        return '???';
-    }
-
-    return $lang_arrays[$language][$form][$text];
-}
-
 function nel_process_i18n($dom, $language = 'en-us')
 {
     if(empty($language))
