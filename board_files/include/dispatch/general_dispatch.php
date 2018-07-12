2ea40b76a5563c063ecba31d2ae18f47128ffb73
OtakuMegane <otakumegane@insaneotaku.com>
2018-07-11 21:57:11 -0400

    Making post in modmode returns to modmode display

--- board_files/include/dispatch/general_dispatch.php
+++ board_files/include/dispatch/general_dispatch.php
@@ -16,19 +16,36 @@ function nel_general_dispatch($inputs)
             if ($inputs['action'] === 'new-post')
             {
                 nel_process_new_post($inputs);
+                $board_references = nel_parameters_and_data()->boardReferences($inputs['board_id']);
 
                 if (nel_fgsfds('noko'))
                 {
-                    echo '<meta http-equiv="refresh" content="1;URL=' .
-                        nel_parameters_and_data()->boardReferences($inputs['board_id'], 'board_directory') . '/' .
-                        nel_parameters_and_data()->boardReferences($inputs['board_id'], 'page_dir') . '/' .
-                        nel_fgsfds('noko_topic') . '/' . nel_fgsfds('noko_topic') . '.html">';
+                    if (nel_sessions()->sessionIsActive())
+                    {
+                        echo '<meta http-equiv="refresh" content="1;URL=' . PHP_SELF .
+                                '?manage=modmode&module=view-thread&section=' . nel_fgsfds('noko_topic') . '&board_id=' .
+                                $inputs['board_id'] . '">';
+                    }
+                    else
+                    {
+                        echo '<meta http-equiv="refresh" content="1;URL=' . $board_references['board_directory'] . '/' .
+                                $board_references['page_dir'] . '/' . nel_fgsfds('noko_topic') . '/' .
+                                nel_fgsfds('noko_topic') . '.html">';
+                    }
                 }
                 else
                 {
-                    echo '<meta http-equiv="refresh" content="1;URL=' .
-                        nel_parameters_and_data()->boardReferences($inputs['board_id'], 'board_directory') . '/' .
-                        PHP_SELF2 . PHP_EXT . '">';
+                    if (nel_sessions()->sessionIsActive())
+                    {
+                        echo '<meta http-equiv="refresh" content="1;URL=' . PHP_SELF .
+                                '?manage=modmode&module=view-index&section=0&board_id=' . $inputs['board_id'] . '">';
+                    }
+                    else
+                    {
+                        echo '<meta http-equiv="refresh" content="1;URL=' .
+                                nel_parameters_and_data()->boardReferences($inputs['board_id'], 'board_directory') . '/' .
+                                PHP_SELF2 . PHP_EXT . '">';
+                    }
                 }
             }
 
