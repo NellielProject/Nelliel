7ed3367c11b2fdb7c810145f903a75ab5dfafb3b
OtakuMegane <otakumegane@insaneotaku.com>
2018-10-14 13:18:02 -0400

    Autoloader update

--- board_files/include/autoload.php
+++ board_files/include/autoload.php
@@ -7,27 +7,19 @@ if (!defined('NELLIEL_VERSION'))
 spl_autoload_register(function ($class)
 {
     $prefix = 'Nelliel\\';
-    $base_dir = __DIR__ . '/';
-    $len = strlen($prefix);
-
-    if (strncmp($prefix, $class, $len) !== 0)
-    {
-        return;
-    }
-
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-
-    if (file_exists($file))
-    {
-        require $file;
-    }
+    $base_directory = __DIR__ . '/';
+    nel_autoload_core($class, $prefix, $base_directory);
 });
 
 spl_autoload_register(function ($class)
 {
     $prefix = 'Nelliel\\';
-    $base_dir = __DIR__ . '/classes/';
+    $base_directory = __DIR__ . '/classes/';
+    nel_autoload_core($class, $prefix, $base_directory);
+});
+
+function nel_autoload_core($class, $prefix, $base_directory)
+{
     $len = strlen($prefix);
 
     if (strncmp($prefix, $class, $len) !== 0)
@@ -36,13 +28,13 @@ spl_autoload_register(function ($class)
     }
 
     $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    $file = $base_directory . str_replace('\\', '/', $relative_class) . '.php';
 
     if (file_exists($file))
     {
         require $file;
     }
-});
+}
 
 require_once LIBRARY_PATH . 'phpDOMExtend/autoload.php';
 require_once LIBRARY_PATH . 'NellielTemplates/autoload.php';
