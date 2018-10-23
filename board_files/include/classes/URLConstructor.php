983f3a816936aad466634305b455cba4ca87604c
OtakuMegane <otakumegane@insaneotaku.com>
2018-10-22 22:33:57 -0400

    Add URLConstructor class

---
+++ board_files/include/classes/URLConstructor.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Nelliel;
+
+if (!defined('NELLIEL_VERSION'))
+{
+    die("NOPE.AVI");
+}
+
+class URLConstructor
+{
+
+    function __construct()
+    {
+    }
+
+    public function staticContent()
+    {
+    }
+
+    public function dynamicContent()
+    {
+    }
+
+    public function dynamicManagement($base_file, $parameters)
+    {
+        $new_url = $base_file . '?';
+
+        foreach ($parameters as $parameter_name => $parameter_value)
+        {
+            $this->appendDynamicParameter($new_url, $parameter_name, $parameter_value);
+        }
+        return $new_url;
+    }
+
+    private function appendDynamicParameter(&$url, $parameter_name, $parameter_value, $encode = true)
+    {
+        $new_parameter = '';
+
+        if (is_null($parameter_value))
+        {
+            return;
+        }
+
+        if (substr($url, -1) !== '?')
+        {
+            $new_parameter .= '&';
+        }
+
+        if ($encode)
+        {
+            $new_parameter .= rawurlencode($parameter_name) . '=' . rawurlencode($parameter_value);
+        }
+        else
+        {
+            $new_parameter .= $parameter_name . '=' . $parameter_value;
+        }
+
+        $url .= $new_parameter;
+    }
+}
