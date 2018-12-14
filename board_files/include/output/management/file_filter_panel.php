edbc9e10298176e43aa56e786ba2f11151d92da4
OtakuMegane <otakumegane@insaneotaku.com>
2018-12-14 13:29:19 -0500

    Tweak file filter panel a bit

--- board_files/include/output/management/file_filter_panel.php
+++ board_files/include/output/management/file_filter_panel.php
@@ -8,7 +8,7 @@ function nel_render_file_filter_panel($user, $domain)
 {
     if (!$user->boardPerm($domain->id(), 'perm_file_filters_access'))
     {
-        nel_derp(341, _gettext('You are not allowed to add file filters.'));
+        nel_derp(341, _gettext('You are not allowed to access the File Filters panel.'));
     }
 
     $database = nel_database();
@@ -36,7 +36,6 @@ function nel_render_file_filter_panel($user, $domain)
 
     $filter_list = $dom->getElementById('filter-list');
     $filter_list_nodes = $filter_list->getElementsByAttributeName('data-parse-id', true);
-    $i = 0;
     $bgclass = 'row1';
 
     foreach ($filters as $filter)
@@ -52,7 +51,6 @@ function nel_render_file_filter_panel($user, $domain)
         $filter_row_nodes['board-id']->setContent($filter['board_id']);
         $remove_link = $url_constructor->dynamic(PHP_SELF, ['manage' => 'general', 'module' => 'file-filter', 'action' => 'remove', 'filter-id' => $filter['entry']]);
         $filter_row_nodes['filter-remove-link']->extSetAttribute('href', $remove_link);
-        $i ++;
     }
 
     $filter_list_nodes['file-filter-row']->remove();
