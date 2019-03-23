9d4c1ff80b3f3082549af82543be7dabaf2b0413
OtakuMegane <otakumegane@insaneotaku.com>
2019-03-23 01:10:44 -0400

    Update bans panel

--- board_files/include/output/OutputPanelBans.php
+++ board_files/include/output/OutputPanelBans.php
@@ -54,10 +54,19 @@ class OutputPanelBans extends OutputCore
     public function renderPanel(array $parameters)
     {
         $user = $parameters['user'];
-        $this->prepare('management/panels/bans_panel_main.html');
+        $final_output = '';
+
+        // Temp
+        $this->render_instance = $this->domain->renderInstance();
+        $this->render_instance->startRenderTimer();
+
         $output_header = new \Nelliel\Output\OutputHeader($this->domain);
         $extra_data = ['header' => _gettext('Board Management'), 'sub_header' => _gettext('Bans')];
-        $output_header->render(['header_type' => 'general', 'dotdot' => '', 'extra_data' => $extra_data]);
+        $final_output .= $output_header->render(['header_type' => 'general', 'dotdot' => '', 'extra_data' => $extra_data]);
+        $template_loader = new \Mustache_Loader_FilesystemLoader($this->domain->templatePath(), ['extension' => '.html']);
+        $render_instance = new \Mustache_Engine(['loader' => $template_loader]);
+        $template_loader->load('management/panels/bans_panel_main');
+        $render_input['can_modify'] = $user->domainPermission($this->domain, 'perm_ban_modify');
 
         if ($this->domain->id() !== '')
         {
@@ -70,42 +79,32 @@ class OutputPanelBans extends OutputCore
             $ban_list = $this->database->executeFetchAll('SELECT * FROM "' . BANS_TABLE . '" ORDER BY "ban_id" DESC', PDO::FETCH_ASSOC);
         }
 
-        $ban_info_table = $this->dom->getElementById('ban-info-table');
-        $ban_info_row = $this->dom->getElementById('ban-info-row');
         $bgclass = 'row1';
 
         foreach ($ban_list as $ban_info)
         {
+            $ban_data = array();
+            $ban_data['bgclass'] = $bgclass;
             $bgclass = ($bgclass === 'row1') ? 'row2' : 'row1';
-            $temp_ban_info_row = $ban_info_row->cloneNode(true);
-            $temp_ban_info_row->extSetAttribute('class', $bgclass);
-            $ban_nodes = $temp_ban_info_row->getElementsByAttributeName('data-parse-id', true);
-            $ban_nodes['ban-id']->setContent($ban_info['ban_id']);
-            $ban_nodes['ban-type']->setContent($ban_info['type']);
-            $ban_nodes['ip-address-start']->setContent(
-                    $ban_info['ip_address_start'] ? @inet_ntop($ban_info['ip_address_start']) : 'Unknown');
-            $ban_nodes['board-id']->setContent($ban_info['board_id']);
-            $ban_nodes['ban-reason']->setContent($ban_info['reason']);
-            $ban_nodes['ban-expiration']->setContent(date("D F jS Y  H:i:s", $ban_info['length'] + $ban_info['start_time']));
-            $ban_nodes['ban-appeal']->setContent($ban_info['appeal']);
-            $ban_nodes['ban-appeal-response']->setContent($ban_info['appeal_response']);
-            $ban_nodes['ban-appeal-status']->setContent($ban_info['appeal_status']);
-            $ban_nodes['link-modify-ban']->extSetAttribute('href',
-                    MAIN_SCRIPT . '?module=board&module=bans&action=modify&ban_id=' . $ban_info['ban_id'] . '&board_id=' .
-                    $this->domain->id());
-            $ban_nodes['link-remove-ban']->extSetAttribute('href',
-                    MAIN_SCRIPT . '?module=board&module=bans&action=remove&ban_id=' . $ban_info['ban_id'] . '&board_id=' .
-                    $this->domain->id());
-            $ban_info_table->appendChild($temp_ban_info_row);
+            $ban_data['ban_id'] = $ban_info['ban_id'];
+            $ban_data['type'] = $ban_info['type'];
+            $ban_data['ip_address_start'] = $ban_info['ip_address_start'] ? @inet_ntop($ban_info['ip_address_start']) : 'Unknown';
+            $ban_data['board_id'] = $ban_info['board_id'];
+            $ban_data['reason'] = $ban_info['reason'];
+            $ban_data['expiration'] = date("D F jS Y  H:i:s", $ban_info['length'] + $ban_info['start_time']);
+            $ban_data['appeal'] = $ban_info['appeal'];
+            $ban_data['appeal_response'] = $ban_info['appeal_response'];
+            $ban_data['appeal_status'] = $ban_info['appeal_status'];
+            $ban_data['modify_url'] = MAIN_SCRIPT . '?module=board&module=bans&action=modify&ban_id=' . $ban_info['ban_id'] . '&board_id=' .
+                    $this->domain->id();
+            $ban_data['remove_url'] = MAIN_SCRIPT . '?module=board&module=bans&action=remove&ban_id=' . $ban_info['ban_id'] . '&board_id=' .
+                    $this->domain->id();
+            $render_input['ban_list'][] = $ban_data;
         }
 
-        $ban_info_row->remove();
+        $render_input['new_ban_url'] = MAIN_SCRIPT . '?module=board&module=bans&action=new&board_id=' . $this->domain->id();
 
-        $form_add_ban = $this->dom->getElementById('link-new-ban');
-        $form_add_ban->extSetAttribute('href', MAIN_SCRIPT . '?module=board&module=bans&action=new&board_id=' . $this->domain->id());
-
-        $this->domain->translator()->translateDom($this->dom);
-        $this->render_instance->appendHTMLFromDOM($this->dom);
+        $this->render_instance->appendHTML($render_instance->render('management/panels/bans_panel_main', $render_input));
         nel_render_general_footer($this->domain);
         echo $this->render_instance->outputRenderSet();
         nel_clean_exit();
@@ -120,42 +119,37 @@ class OutputPanelBans extends OutputCore
             nel_derp(321, _gettext('You are not allowed to modify bans.'));
         }
 
-        $this->prepare('management/panels/bans_panel_add_ban.html');
+        $final_output = '';
+
+        // Temp
+        $this->render_instance = $this->domain->renderInstance();
+        $this->render_instance->startRenderTimer();
+
         $output_header = new \Nelliel\Output\OutputHeader($this->domain);
         $extra_data = ['header' => _gettext('Board Management'), 'sub_header' => _gettext('Add Ban')];
-        $output_header->render(['header_type' => 'general', 'dotdot' => '', 'extra_data' => $extra_data]);
+        $final_output .= $output_header->render(['header_type' => 'general', 'dotdot' => '', 'extra_data' => $extra_data]);
+        $template_loader = new \Mustache_Loader_FilesystemLoader($this->domain->templatePath(), ['extension' => '.html']);
+        $render_instance = new \Mustache_Engine(['loader' => $template_loader]);
+        $template_loader->load('management/panels/bans_panel_add');
+        $render_input['ban_board'] = (!empty($this->domain->id())) ? $this->domain->id() : '';
         $ip = $parameters['ip'];
         $type = $parameters['type'];
 
-        if (!$user->domainPermission($this->domain, 'perm_ban_modify'))
-        {
-            $this->dom->getElementById('ban-all-boards-row')->remove();
-        }
-
-        if (!empty($this->domain->id()))
-        {
-            $add_ban_form = $this->dom->getElementById('ban-board-field')->extSetAttribute('value', $this->domain->id());
-        }
-
         if ($type === 'POST' && isset($_GET['post-id']))
         {
+            $render_input['is_post_ban'] = true;
             $post_param = '&post-id=' . $_GET['post-id'];
         }
         else
         {
             $post_param = '';
-            $this->dom->getElementById('ban-mod-comment')->remove();
         }
 
-        $add_ban_form = $this->dom->getElementById('add-ban-form');
-        $add_ban_form->extSetAttribute('action',
-                MAIN_SCRIPT . '?module=board&module=bans&action=add&board_id=' . $this->domain->id() . $post_param);
-        $ban_nodes = $add_ban_form->getElementsByAttributeName('data-parse-id', true);
-        $ban_nodes['ban-ip']->extSetAttribute('value', $ip);
-        $this->dom->getElementById('ban-type')->extSetAttribute('value', $type);
+        $render_input['form_action'] = MAIN_SCRIPT . '?module=board&module=bans&action=add&board_id=' . $this->domain->id() . $post_param;
+        $render_input['ban_ip'] = $ip;
+        $render_input['ban_type'] = $type;
 
-        $this->domain->translator()->translateDom($this->dom);
-        $this->render_instance->appendHTMLFromDOM($this->dom);
+        $this->render_instance->appendHTML($render_instance->render('management/panels/bans_panel_add', $render_input));
         nel_render_general_footer($this->domain);
         echo $this->render_instance->outputRenderSet();
         nel_clean_exit();
@@ -170,61 +164,41 @@ class OutputPanelBans extends OutputCore
             nel_derp(321, _gettext('You are not allowed to modify bans.'));
         }
 
-        $ban_hammer = new \Nelliel\BanHammer($this->domain->database());
-        $this->prepare('management/panels/bans_panel_modify_ban.html');
+        $final_output = '';
+
+        // Temp
+        $this->render_instance = $this->domain->renderInstance();
+        $this->render_instance->startRenderTimer();
+
         $output_header = new \Nelliel\Output\OutputHeader($this->domain);
         $extra_data = ['header' => _gettext('Board Management'), 'sub_header' => _gettext('Modify Ban')];
-        $output_header->render(['header_type' => 'general', 'dotdot' => '', 'extra_data' => $extra_data]);
-        $this->dom->getElementById('modify-ban-form')->extSetAttribute('action',
-                MAIN_SCRIPT . '?module=board&module=bans&action=update&board_id=' . $this->domain->id());
+        $final_output .= $output_header->render(['header_type' => 'general', 'dotdot' => '', 'extra_data' => $extra_data]);
+        $template_loader = new \Mustache_Loader_FilesystemLoader($this->domain->templatePath(), ['extension' => '.html']);
+        $render_instance = new \Mustache_Engine(['loader' => $template_loader]);
+        $template_loader->load('management/panels/bans_panel_modify');
+        $render_input['form_action'] = MAIN_SCRIPT . '?module=board&module=bans&action=update&board_id=' . $this->domain->id();
         $ban_id = $_GET['ban_id'];
+        $ban_hammer = new \Nelliel\BanHammer($this->database);
         $ban_info = $ban_hammer->getBanById($ban_id, true);
-        $this->dom->getElementById('ban-ip-field')->extSetAttribute('value', @inet_ntop($ban_info['ip_address_start']));
-        $this->dom->getElementById('ban-board-field')->setContent($ban_info['board_id']);
-        $this->dom->getElementById('ban-type-display')->setContent($ban_info['type']);
-        $this->dom->getElementById('ban-time-display')->setContent(date("D F jS Y  H:i:s", $ban_info['start_time']));
-        $this->dom->getElementById('ban-expiration-display')->setContent(
-                date("D F jS Y  H:i:s", $ban_info['length'] + $ban_info['start_time']));
-        $this->dom->getElementById('ban-time-years')->extSetAttribute('value', $ban_info['years']);
-        $this->dom->getElementById('ban-time-days')->extSetAttribute('value', $ban_info['days']);
-        $this->dom->getElementById('ban-time-hours')->extSetAttribute('value', $ban_info['hours']);
-        $this->dom->getElementById('ban-time-minutes')->extSetAttribute('value', $ban_info['minutes']);
+        $render_input['ban_id'] = $ban_info['ban_id'];
+        $render_input['ip_address_start'] = inet_ntop($ban_info['ip_address_start']);
+        $render_input['board_id'] = $ban_info['board_id'];
+        $render_input['type'] = $ban_info['type'];
+        $render_input['start_time_formatted'] = date("D F jS Y  H:i:s", $ban_info['start_time']);
+        $render_input['expiration'] = date("D F jS Y  H:i:s", $ban_info['length'] + $ban_info['start_time']);
+        $render_input['years'] = $ban_info['years'];
+        $render_input['days'] = $ban_info['days'];
+        $render_input['hours'] = $ban_info['hours'];
+        $render_input['minutes'] = $ban_info['minutes'];
+        $render_input['all_boards'] = ($ban_info['all_boards'] > 0) ? 'checked' : '';
+        $render_input['start_time'] = $ban_info['start_time'];
+        $render_input['reason'] = $ban_info['reason'];
+        $render_input['creator'] = $ban_info['creator'];
+        $render_input['appeal'] = $ban_info['appeal'];
+        $render_input['appeal_response'] = $ban_info['appeal_response'];
+        $render_input['appeal_status'] = ($ban_info['appeal_status'] > 1) ? 'checked' : '';
 
-        if (($ban_info['all_boards'] > 0))
-        {
-            $this->dom->getElementById('ban-all-boards-field')->extSetAttribute('checked', true);
-        }
-
-        $this->dom->getElementById('ban-id-field')->extSetAttribute('value', $ban_info['ban_id']);
-        $this->dom->getElementById('ban-start-field')->extSetAttribute('value', $ban_info['start_time']);
-        $this->dom->getElementById('ban-reason-field')->setContent($ban_info['reason']);
-        $this->dom->getElementById('ban-name-display')->setContent($ban_info['creator']);
-
-        if ($ban_info['appeal'] === '')
-        {
-            $this->dom->getElementById('ban-appeal-display-row')->remove();
-        }
-        else
-        {
-            $this->dom->getElementById('ban-appeal-display')->setContent($ban_info['appeal']);
-        }
-
-        if ($ban_info['appeal_response'] === '')
-        {
-            $this->dom->getElementById('ban-appeal-response-row')->remove();
-        }
-        else
-        {
-            $this->dom->getElementById('ban-appeal-response')->setContent($ban_info['appeal_response']);
-        }
-
-        if ($ban_info['appeal_status'] > 1)
-        {
-            $this->dom->getElementById('ban-appealed-field')->extSetAttribute('checked', 'checked');
-        }
-
-        $this->domain->translator()->translateDom($this->dom);
-        $this->render_instance->appendHTMLFromDOM($this->dom);
+        $this->render_instance->appendHTML($render_instance->render('management/panels/bans_panel_modify', $render_input));
         nel_render_general_footer($this->domain);
         echo $this->render_instance->outputRenderSet();
         nel_clean_exit();
