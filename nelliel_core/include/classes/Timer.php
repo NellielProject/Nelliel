058ecb23ae995aaca8b68a0f0e966796a1fdf46f
OtakuMegane <otakumegane@insaneotaku.com>
2021-01-06 01:17:13 -0500

    Added timer class

---
+++ nelliel_core/include/classes/Timer.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Nelliel;
+
+if (!defined('NELLIEL_VERSION'))
+{
+    die("NOPE.AVI");
+}
+
+class Timer
+{
+    protected $start = 0;
+    protected $elapsed = 0;
+    protected $stopped = true;
+
+    function __construct()
+    {
+    }
+
+    public function start()
+    {
+        if ($this->stopped)
+        {
+            $this->start = microtime(true);
+            $this->stopped = false;
+        }
+    }
+
+    public function stop()
+    {
+        if (!$this->stopped)
+        {
+            $this->elapsed += microtime(true) - $this->start;
+            $this->start = 0;
+            $this->stopped = true;
+        }
+    }
+
+    public function reset()
+    {
+        $this->start = 0;
+        $this->elapsed = 0;
+        $this->stopped = false;
+    }
+
+    public function elapsed(bool $rounded = true, int $precision = 4)
+    {
+        $elapsed = $this->stopped ? $this->elapsed : (microtime(true) - $this->start) + $this->elapsed;
+
+        if ($rounded)
+        {
+            return number_format($elapsed, $precision);
+        }
+        else
+        {
+            return $elapsed;
+        }
+    }
+}
