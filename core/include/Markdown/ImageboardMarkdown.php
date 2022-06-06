b23cc66eb638b8105037974dabf2d99725ec2f9e
OtakuMegane <otakumegane@insaneotaku.com>
2022-06-06 17:42:44 -0400

    Moved quote and cite markdown to trait

--- core/include/Markdown/ImageboardMarkdown.php
+++ core/include/Markdown/ImageboardMarkdown.php
@@ -18,6 +18,8 @@ class ImageboardMarkdown extends Parser
     use WhitespaceLine;
     use URL;
     use Spoiler;
+    use QuoteAndCite;
+
     protected $domain;
     protected $url_protocols;
     protected $cites;
@@ -32,7 +34,8 @@ class ImageboardMarkdown extends Parser
         $this->post_content_id = $post_content_id;
     }
 
-    public function parseDynamic(string $text) {
+    public function parseDynamic(string $text)
+    {
         $original = $this->dynamic;
         $this->dynamic = true;
         $this->parse($text);
@@ -127,45 +130,5 @@ class ImageboardMarkdown extends Parser
         }
 
         return $markers;
-    }
-
-    /**
-     * Parses quotes and content cites
-     *
-     * @marker >
-     */
-    protected function parseQuoteAndContentCite(string $text): array
-    {
-        $quote_regex = '/^>(?:(?!>\d+|>>\/\w+\/))(.*)/iu';
-        $matches = array();
-
-        if (preg_match($quote_regex, $text, $matches) === 1) {
-            return [['quote', $this->parseInline($matches[1])], utf8_strlen($matches[0])];
-        } else {
-            $cite_type = $this->cites->citeType($text);
-
-            if ($cite_type['type'] !== 'not-cite') {
-                return [['contentcite', $cite_type['matches'][0]], utf8_strlen($cite_type['matches'][0])];
-            }
-
-            return [['text', '>'], 1];
-        }
-    }
-
-    protected function renderQuote(array $block): string
-    {
-        return '<span class="quote">>' . $this->renderAbsy($block[1]) . '</span>';
-    }
-
-    protected function renderContentCite(array $block): string
-    {
-        $cite_data = $this->cites->getCiteData($block[1], $this->domain, $this->post_content_id);
-
-        if (isset($cite_data['exists']) && $cite_data['exists']) {
-            $cite_url = $this->cites->createPostLinkURL($cite_data, $this->domain, $this->dynamic);
-            return '<a href="' . $cite_url . '" class="post-cite" data-command="show-linked-post">' . $block[1] . '</a>';
-        } else {
-            return '<s class="invalid-cite">' . $block[1] . '</s>';
-        }
     }
 }
