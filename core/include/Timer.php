4b181ba698c366303d3fcc5c521702e3ac8753f3
OtakuMegane <otakumegane@insaneotaku.com>
2022-08-02 15:05:49 -0400

    Updated Timer class

--- core/include/Timer.php
+++ core/include/Timer.php
@@ -1,6 +1,5 @@
 <?php
-
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Nelliel;
 
@@ -13,46 +12,55 @@ class Timer
     protected $stopped = true;
 
     function __construct()
-    {
-    }
+    {}
 
-    public function start()
+    /**
+     * Start the timer.
+     */
+    public function start(): void
     {
-        if ($this->stopped)
-        {
+        if ($this->stopped) {
             $this->start = microtime(true);
             $this->stopped = false;
         }
     }
 
-    public function stop()
+    /**
+     * Stop the timer and update the elapsed time.
+     */
+    public function stop(): void
     {
-        if (!$this->stopped)
-        {
+        if (!$this->stopped) {
             $this->elapsed += microtime(true) - $this->start;
             $this->start = 0;
             $this->stopped = true;
         }
     }
 
-    public function reset()
+    /**
+     * Stop the timer and reset elapsed time.
+     */
+    public function reset(): void
     {
         $this->start = 0;
         $this->elapsed = 0;
         $this->stopped = true;
     }
 
-    public function elapsed(bool $rounded = true, int $precision = 4)
+    /**
+     * Get the elapsed time as a float.
+     */
+    public function elapsed(): float
     {
-        $elapsed = $this->stopped ? $this->elapsed : (microtime(true) - $this->start) + $this->elapsed;
+        return $this->stopped ? $this->elapsed : (microtime(true) - $this->start) + $this->elapsed;
+    }
 
-        if ($rounded)
-        {
-            return number_format($elapsed, $precision);
-        }
-        else
-        {
-            return $elapsed;
-        }
+    /**
+     * Get the elapsed time as a string formatted to the given precision.
+     * Default precision is 4.
+     */
+    public function elapsedFormatted(int $precision = 4): string
+    {
+        return number_format($this->elapsed(), $precision);
     }
 }
